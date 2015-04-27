09a7f999681b4d4e3b8cc50baecd62ff650018f7
Mona <mona.m.6@web.de>
2015-04-27 11:49:26 +0200

    Slingshot - Hover Done

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,25 @@
+﻿ using UnityEngine;
+using System.Collections;
+
+public class Slingshot : MonoBehaviour {
+
+	public GameObject launchPoint;
+
+	void Awake() {
+		Transform LaunchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = LaunchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+
+	}
+
+	void OnMouseEnter() {
+		print ("Yo!");
+		launchPoint.SetActive(true);
+	}
+
+	void OnMouseExit() {
+		print ("no!");
+		launchPoint.SetActive(false);
+
+	}
+}
