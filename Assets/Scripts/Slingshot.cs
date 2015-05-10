b4e0b7265139df5cdc1113a1e3bb83c4369ebdea
Mona <mona.m.6@web.de>
2015-05-10 22:15:23 +0200

    slingshot -> halo bleibt im aiming mode auch wenn maus draußen ist

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
-	public GameObject launchPoint;
+
+	//Inspector fields
+	public GameObject prefabProjectile;
+	public float velocityMult;
+
+
+	//Internal fields
+	private GameObject launchPoint;
+	private bool aimingMode;
+
+	private Vector3 launchPos;
+	private GameObject projectile;
+
 
 	void Awake() {
-		Transform LaunchPointTrans = transform.Find ("LaunchPoint");
-		launchPoint = LaunchPointTrans.gameObject;
+		Transform launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
 		launchPoint.SetActive(false);
-
+		launchPos = launchPoint.transform.position;
 	}
 
 	void OnMouseEnter() {
@@ -19,7 +31,55 @@ public class Slingshot : MonoBehaviour {
 
 	void OnMouseExit() {
 		print ("no!");
-		launchPoint.SetActive(false);
+		if(aimingMode != true){
+			launchPoint.SetActive(false);}
+
+
+	}
+
+	void OnMouseDown(){
+		//set aim to aiminng modeee!
+		aimingMode = true;
+
+		//instantiate project tile
+		projectile = Instantiate(prefabProjectile) as GameObject;
+
+		//position project tile at launchpoint
+		projectile.transform.position = launchPos;
+
+		//disable physics
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
+	}
+
+	void Update(){
+		//check aiming mode
+		if (!aimingMode) return;
+
+		//mouse pos 3D space
+		Vector3 mousePos2D = Input.mousePosition;
+		mousePos2D.z = -Camera.main.transform.position.z;
+		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+		//calculate delta btw launchPos and mouse pos
+		Vector3 mouseDelta = mousePos3D - launchPos;
+
+		//constrain the delta to radius of the sphere collider
+		float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+		mouseDelta = Vector3.ClampMagnitude(mouseDelta, maxMagnitude);
+
+		//set projectile to new pos
+		Vector3 projPos = launchPos + mouseDelta;
+		projectile.transform.position = projPos;
+
+		//check mouse released
+		if (Input.GetMouseButtonUp(0)){
+			aimingMode = false;
+			projectile.GetComponent<Rigidbody>().isKinematic = false;
+			projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+
+		}
+		  //fire it OFF
+
 
 	}
 }
