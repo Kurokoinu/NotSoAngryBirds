2622dea33530d015777318a34275f7d2659753e4
Mona <mona.m.6@web.de>
2015-07-12 10:16:32 +0200

    Bomb Projectile Script

---
+++ Assets/Scripts/BombBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombBall : MonoBehaviour {
+
+	public GameObject Particle_Bomb;
+	
+	
+	void OnTriggerEnter(Collider other) {
+		
+		if(other.tag == "castle" || other.tag == "floor"){
+			Instantiate(Particle_Bomb, this.transform.position, this.transform.rotation);
+			Destroy(GameObject.FindGameObjectWithTag("castle"));
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/BombDestroy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombDestroy : MonoBehaviour {
+
+	public GameObject Particle_Bomb;
+	
+	
+	void OnTriggerEnter(Collider other) {
+		
+		if(other.tag == "bomb"){
+			Instantiate(Particle_Bomb, this.transform.position, this.transform.rotation);
+			Destroy(this.gameObject);
+		}
+	}
+}
